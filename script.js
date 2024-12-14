f862d85fc2ca04840985d3a658ba290a4e6e1b3a
Tan-Vir-alam <tanviralam750738@gmail.com>
2024-12-14 13:11:21 +0600

    Add a counter app

---
+++ script.js
@@ -0,0 +1,18 @@
+let count = 0;
+const counterElement = document.getElementById("counter");
+const decrementButton = document.getElementById("decrement");
+const incrementButton = document.getElementById ("increment");
+
+function increaseCount(){
+count = count + 1;
+counterElement.innerText = count
+
+}
+
+function decreaseCount(){
+    count = count - 1
+    counterElement.innerText = count
+}
+
+incrementButton.addEventListener("click", increaseCount);
+decrementButton.addEventListener("click", decreaseCount);
